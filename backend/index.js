1253286825ba495d2ecb71fb20ae98966aa1c2be
Isuru67 <githubisuru4@gmail.com>
2024-12-30 08:39:01 +0530

    routes added
    routes added

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import { PORT, mongoDBURL } from "./config.js";
-import { Booking } from "./models/bookingModel.js";
-
-
-
+import bookingRoutes from "./routes/bookingRoutes.js";
 
 
 
@@ -12,114 +9,7 @@ const app = express();
 
 app.use(express.json());
 
-
-
-
-app.post('/nbooking', async (request , response) => {
-
-
-const bookingDetails = new Booking(request.body);
-
-try{
-await bookingDetails.save();
-response.status(201).send(bookingDetails);
-
-}
-catch (error){
-
-    console.error("Error placing order:", error);
-    response.status(500).json({ message: "Failed to place Booking." });
-
-
-
-}
-
-});
-
-app.get('/nbooking',async (request,response) => {
-
-    try{
-    const bookings = await Booking.find({});
-    return response.status(200).json(bookings);
-    
-    }catch(error){
-    
-    
-    console.log(error.message);
-    response.status(500).send({ message: error.message});
-    
-    
-    }
-    
-    
-    });
-
-    app.get('/nbooking/:id',async (request,response) => {
-
-        try{
-    
-        const {id} = request.params;
-        const booking = await Booking.findById(id);
-        return response.status(200).json(booking);
-            
-        }catch(error){
-            
-            
-        console.log(error.message);
-        response.status(500).send({ message: error.message});
-            
-            
-        }
-            
-            
-        });
-
-app.get('/nbooking/:cusId',async (request,response) => {
-
-    try{
-
-    const {cusId} = request.params;
-    const booking = await Booking.findOne({cusId});
-    return response.status(200).json(booking);
-        
-    }catch(error){
-        
-        
-    console.log(error.message);
-    response.status(500).send({ message: error.message});
-        
-        
-    }
-        
-        
-    });
-
-app.put('/nbooking/:id', async (request , response) =>{
-
-try {
-
-const {id} = request.params;
-const result = await Booking.findByIdAndUpdate(id, request.body);
-
-if(!result){
-    return response.status(404).json({ message: 'Booking not found'});
-
-}
-
-return response.status(200).send({message : 'Booking Updated Successfully'});
-    
-} catch (error) {
-    
- console.log(error.message);
- response.status(500).send({ message: error.message});
-
-}
-
-
-})
-
-
-
+app.use('/nbooking' , bookingRoutes);
 
 
 mongoose
